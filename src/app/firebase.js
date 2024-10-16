c861cca55f743996f46c969763c9d987b62ded33
ShaheerSyed27 <shaheer.iiee@gmail.com>
2024-10-17 08:11:00 +1300

    global - firebase config issues

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,21 +1,24 @@
 // src/app/firebase.js
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Use getAuth instead of auth
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyAEuHdl1bFn9eYP1XU_L2xgsH86kQnDQog",
-  authDomain: "duplex-brothers.firebaseapp.com",
-  projectId: "duplex-brothers",
-  storageBucket: "duplex-brothers.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdefghij1234567"
-};
+    apiKey: "AIzaSyAEuHdllbFn9eYPlXU_L2xgsH86kQnDQog",
+    authDomain: "duplex-brothers.firebaseapp.com",
+    projectId: "duplex-brothers",
+    storageBucket: "duplex-brothers.appspot.com",
+    messagingSenderId: "1094347599238",
+    appId: "1:1094347599238:web:be65d1842d78bb67e39007",
+    measurementId: "G-S0200Z5BHL"
+  };
 
 // Initialize Firebase only if it hasn't been initialized already
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Export the Firebase services
 export const auth = getAuth(app);  // Correct export of auth
