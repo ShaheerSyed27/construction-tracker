7cbc4825a173f14e0f84963f8bc9e87edd6d6e15
ShaheerSyed27 <shaheer.iiee@gmail.com>
2024-10-17 03:19:31 +1300

    homepage addition

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,10 @@
+// src/app/HomePage.tsx
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Duplex Brothers</h1>
+        <p>Track your construction project easily.</p>
+      </div>
+    );
+  }
+  
